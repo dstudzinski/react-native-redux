35b3178de8e3ce18b4639fe8b25c34cc380493d1
Dawid Studziński <dawid@compsolutions.pl>
2016-09-05 21:46:18 +0200

    persistent storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import devTools from 'remote-redux-devtools';
+import {persistStore, autoRehydrate} from 'redux-persist'
+import {AsyncStorage} from 'react-native'
 
 import rootReducer from './rootReducer';
 
 export default function configureStore(initialState) {
   const enhancer = compose(
+    autoRehydrate(),
     applyMiddleware(thunk),
     devTools({ realtime: true })
   );
   const store = createStore(rootReducer, initialState, enhancer);
 
+  persistStore(store,{storage: AsyncStorage});
   devTools.updateStore(store);
 
   return store;
