123e5278e44c4ebf384033118becbe55c7314353
Dawid Studziński <dawid@compsolutions.pl>
2016-09-17 11:30:05 +0200

    drawer (side menu)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {
   StyleSheet,
-  Navigator
+  Navigator,
+  Text,
+  StatusBar
 } from 'react-native';
+import Drawer from 'react-native-drawer';
 
 import ROUTES from '../routes';
 
@@ -13,12 +16,33 @@ const App = () => {
   };
 
   return (
-    <Navigator
-      style={styles.container}
-      initialRoute={{name: 'newProcedureView'}}
-      renderScene={renderScene}
-      configureScene={() => {return Navigator.SceneConfigs.FloatFromRight;}}
-    />
+  <Drawer
+    ref={(ref) => this._drawer = ref}
+    type="overlay"
+    content={<Text>some menu items</Text>}
+    tapToClose={true}
+    open={true}
+    openDrawerOffset={0.2}
+    panCloseMask={0.2}
+    closedDrawerOffset={-3}
+    styles={{
+      drawer: {shadowColor: '#ff0000', shadowOpacity: 0.8, shadowRadius: 3, backgroundColor: '#ff0000'},
+      main: {paddingLeft: 3}
+    }}
+    tweenHandler={(ratio) => ({
+      main: { opacity:(2-ratio)/2 }
+    })}>
+      <StatusBar
+        backgroundColor="blue"
+        barStyle="light-content"
+      />
+      <Navigator
+        style={styles.container}
+        initialRoute={{name: 'newProcedureView'}}
+        renderScene={renderScene}
+        configureScene={() => {return Navigator.SceneConfigs.FadeAndroid;}}
+      />
+  </Drawer>
   );
 };
 
